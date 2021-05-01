ab2d61bceb654cb1bd2965b4c9cd65404e6f1422
tetianako-prog <t.kobyliakova@gmail.com>
2021-05-01 12:38:07 +0300

    Use styled-components

--- src/components/contacts/ContactItem.js
+++ src/components/contacts/ContactItem.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import './ContactItem.scss';
+import styled from 'styled-components';
 
-const ContactItem = ({ info, onDeleteContact }) => {
+const ContactItemStyled = styled.li`
+  font-size: 18px;
+  margin-left: 20px;
+  margin-bottom: 10px;
+`;
+
+const DeleteButtonStyled = styled.button`
+  display: inline-block;
+  border: none;
+  font: inherit;
+  cursor: pointer;
+  outline: none;
+  margin-left: 20px;
+  padding-right: 8px;
+  padding-left: 8px;
+  font-size: 14px;
+  background-color: #dcdcdc;
+  border: 1px solid #696969;
+  border-radius: 5px;
+  &:hover {
+    background-color: #808080;
+    color: #fff;
+  }
+`;
+
+const ContactItem = ({ info: { name, number, id }, onDeleteContact }) => {
   return (
-    <li className="ContactItem">
-      {info.name}: {info.number}
-      <button onClick={() => onDeleteContact(info.id)}>Delete</button>
-    </li>
+    <ContactItemStyled>
+      {name}: {number}
+      <DeleteButtonStyled onClick={() => onDeleteContact(id)}>
+        Delete
+      </DeleteButtonStyled>
+    </ContactItemStyled>
   );
 };
 
--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { v4 } from 'uuid';
-import './Filter.scss';
+import styled from 'styled-components';
+
+const FilterStyled = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  font-size: 18px;
+  margin-bottom: 20px;
+  .label {
+    margin-bottom: 10px;
+  }
+`;
 
 const Filter = ({ value, onChange }) => {
   const filterId = v4();
   return (
-    <div className="Filter">
-      <label htmlFor={filterId}>Find contacts by name</label>
+    <FilterStyled>
+      <label className="label" htmlFor={filterId}>
+        Find contacts by name
+      </label>
       <input type="text" value={value} onChange={onChange} id={filterId} />
-    </div>
+    </FilterStyled>
   );
 };
 
